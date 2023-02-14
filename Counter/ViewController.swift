d90532bc0a97c0f9c6bbff10eff7fde0a9b9c40b
Илья Харитонов <ilya_kharitonov@icloud.com>
2023-02-14 16:29:15 +0300

    В ViewController добавлен метод для подсчёта количества нажатий на кнопку

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,12 +8,18 @@
 import UIKit
 
 class ViewController: UIViewController {
+    private var counter: Int = 0
 
+    @IBOutlet weak var labelWithCounter: UILabel!
+    @IBOutlet weak var buttonUpdateCounter: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func buttonDidTap(_ sender: Any) {
+        counter += 1
+        labelWithCounter.text = "Значение счётчика:  \(counter)"
+    }
 }
 
