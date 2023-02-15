bd1e028eec3a1d938c37c251b0d34c6d32a2b62f
Илья Харитонов <ilya_kharitonov@icloud.com>
2023-02-15 12:24:40 +0300

    В ViewController добавлены модификаторы доступа для переменных и функции. Удалена @IBOutlet для Button. В Storyboard убрана связь для @IBOutlet.

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -1,25 +1,17 @@
-//
-//  ViewController.swift
-//  Counter
-//
-//  Created by Харитонов Илья on 13.02.2023.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
     private var counter: Int = 0
 
-    @IBOutlet weak var labelWithCounter: UILabel!
-    @IBOutlet weak var buttonUpdateCounter: UIButton!
+    
+    @IBOutlet weak private var counterLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-    @IBAction func buttonDidTap(_ sender: Any) {
+    
+    @IBAction private func didTapButton(_ sender: Any) {
         counter += 1
-        labelWithCounter.text = "Значение счётчика:  \(counter)"
+        counterLabel.text = "Значение счётчика: \(counter)"
     }
 }
 
